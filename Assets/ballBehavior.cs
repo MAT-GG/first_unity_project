f1997af043a28bee9324517b84da082a2a2d5d7b
Hannah <hanawolfe@gmail.com>
2016-10-12 12:03:28 -0700

    project started with ball

---
+++ Assets/ballBehavior.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ballBehavior : MonoBehaviour {
+
+    public int count;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("start");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void OnCollisionEnter(Collision col){
+        if (col.gameObject.name == "Cube")
+        {
+            Debug.Log("hit floor");
+            Vector3 temp = transform.position;
+            temp.y = 10.0f;
+            transform.position = temp;
+            count++;
+        }
+    }
+}
