0e9b092f222df8c30b29164f2ff8b35e12503b3d
MAT Graphics Group <mat.graphics.group@gmail.com>
2016-11-03 17:21:07 -0700

    kitty jpg git lfs tracked

---
+++ Assets/shadertest/NewBehaviourScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //GetComponent<Renderer>().material.SetTexture("Texture", Resources.Load("kitty") as Texture);
+        GetComponent<Renderer>().material.mainTexture = Resources.Load("kitty") as Texture;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
